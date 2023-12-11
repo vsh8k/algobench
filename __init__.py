0b060d3f9d94f6c246392a56a68e0d8b31b19db0
Eimantas Kaušakys <75587615+vsh8k@users.noreply.github.com>
2023-12-12 01:02:58 +0200

    Working demo for presentation

--- __init__.py
+++ __init__.py
@@ -1,30 +1,75 @@
-import sort
+from progress.bar import ShadyBar
+from progress.spinner import PixelSpinner
 import time
+import sort
 import random
 import numpy as np
+
 class data:
     def __init__(self, count) -> None:
         self.array = []
         for x in range(count):
             self.array.append([])
     def generateRand(self, size, min, max):
+        it = 0
         for element in self.array:
-            for x in range(size):
-                element.append(random.randint(min, max))
+            with ShadyBar(f'Generating random numbers for array #{it}:', max=size) as bar:
+                for x in range(size):
+                    element.append(random.randint(min, max))
+                    bar.next();
+            it += 1
     def partialSort(self, x):
         for i in range(len(self.array)):
+            spinner = PixelSpinner(f'Partially sorting array #{i}')
             element = self.array[i]
             index_to_sort = int(len(element) * x)
             self.array[i] = np.partition(element, index_to_sort)
+            spinner.next()
 
     def print(self):
         print(self.array)
 
-test = data(1)
+print("Shell Sort Test")
+test = data(5)
+avg_shell = [0, 0, 0, 0, 0]
+for i in range(1, 6):#5 skirtingi duomenų kiekiai
+    test.generateRand(i * 10, 0, 10)
+    for x in range(6): #Bandymus atliekame 6 kartus
+        spinner = PixelSpinner(f'Sorting with Shellsort size:{i*1000}, attempt:{x+1}...')
+        for x in test.array:
+            start_time = time.time()
+            sort.shellSort(x, spinner)
+            end_time = time.time()
+            diff = end_time - start_time
+            print(f"\nTime taken to sort using Shellsort: {diff}") 
+            avg_shell[i-1] += diff
+        spinner.finish()
+    avg_shell[i-1] /= 6
+print(f"Average sorting times for Shellsort: {avg_shell}")
 
-test.generateRand(10, 0, 10)
-print(test.array)
-test.partialSort(0.95)
-test.print()
+print("Quicksort Test")
 
-#print(sort.shellSort(arr))
+test_quick = data(5)
+
+size = 20000000
+
+test_quick.generateRand(size, 55, 100)
+
+test_quick.partialSort(0.95)
+
+avg_quick = 0;
+
+for x in range(len(test_quick.array)): #Bandymus atliekame 6 kartus
+        spinner = PixelSpinner(f'Sorting with Quicksort size:{size}, attempt:{x+1}...')
+        start_time = time.time()
+        sort.quick_sort(test_quick.array[x], 0, len(test_quick.array[x])-1)
+        end_time = time.time()
+        diff = end_time - start_time
+        print(f"\nTime taken to sort using Quicksort: {diff}") 
+        avg_quick += diff
+        spinner.finish()
+        
+avg_quick /= 6
+
+print(f"Average Quicksort sorting time: {avg_quick}")
+
--- sort.py
+++ sort.py
@@ -1,3 +1,4 @@
+from progress.spinner import PixelSpinner
 # Python program for implementation of Quicksort Sort
 
 # This implementation utilizes pivot as the last element in the nums list
@@ -7,64 +8,25 @@
 
 
 # Function to find the __partition position
-def __partition(array, low, high):
-
-	# choose the rightmost element as pivot
-	pivot = array[high]
-
-	# pointer for greater element
-	i = low - 1
-
-	# traverse through all elements
-	# compare each element with pivot
-	for j in range(low, high):
-		if array[j] <= pivot:
-
-			# If element smaller than pivot is found
-			# swap it with the greater element pointed by i
-			i = i + 1
-
-			# Swapping element at i with element at j
-			(array[i], array[j]) = (array[j], array[i])
-
-	# Swap the pivot element with the greater element specified by i
-	(array[i + 1], array[high]) = (array[high], array[i + 1])
-
-	# Return the position from where __partition is done
-	return i + 1
-
-# function to perform quicksort
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low,high):
+        if array[j] < pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+            
+    array[i+1], array[high] = array[high], array[i+1]
+    return i + 1, array
 
 
-def quickSort(array, low, high):
-	if low < high:
-
-		# Find pivot element such that
-		# element smaller than pivot are on the left
-		# element greater than pivot are on the right
-		pi = __partition(array, low, high)
-
-		# Recursive call on the left of pivot
-		quickSort(array, low, pi - 1)
-
-		# Recursive call on the right of pivot
-		quickSort(array, pi + 1, high)
-
-
-#data = [1, 7, 4, 1, 10, 9, -2]
-#print("Unsorted Array")
-#print(data)
-#
-#size = len(data)
-#
-#quickSort(data, 0, size - 1)
-#
-#print('Sorted Array in Ascending Order:')
-#print(data)
-
-
-
-
+def quick_sort(array, low, high):
+    if low < high:
+        partition_index, array = partition(array, low, high)
+        quick_sort(array, low, partition_index-1)
+        quick_sort(array, partition_index+1, high)
+    
+    return array
 
 
 # Python program for implementation of Shell Sort
@@ -80,12 +42,13 @@ def __sedgewick_gaps(arr_length):
         k += 1
     return gaps
 
-def shellSort(arr):
+def shellSort(arr, spinner):
     n = len(arr)
     gaps = __sedgewick_gaps(n)
     
     for gap in reversed(gaps):
         for i in range(gap, n):
+            spinner.next()
             temp = arr[i]
             j = i
             while j >= gap and arr[j - gap] > temp:
